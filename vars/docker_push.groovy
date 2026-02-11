de2d8c451ee1274654746b7af73034660d2a26f9
vodaassure <vodaassure@gmail.com>
2026-02-11 06:57:43 +0000

    push

---
+++ vars/docker_push.groovy
@@ -0,0 +1,10 @@
+def call(String credId String imageName){
+    withCredentials([usernamePassword(credentialsId:${"credId"},
+                    passwordVariable: "dockerhubPass",
+                    usernameVariable: "dockerhubUser"
+                )]){
+
+                    sh "docker login -u ${env.dockerhubUser} -p ${env.dockerhubPass}"
+                    sh "docker tag ${"imageName"} ${env.dockerhubUser}/${"imageName"}"
+                    sh "docker push ${env.dockerhubUser}/${"imageName"}:latest"
+}
